781a26725b6b7563d900d681e6ddc08f17f66ab3
cloudman549 <cloudman549@gmail.com>
2025-08-15 07:57:54 +0530

    AUTHOR

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def get_active_app():
     except Exception as e:
         return jsonify({"status": False, "message": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
